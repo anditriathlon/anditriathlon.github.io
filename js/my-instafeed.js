93dc0a20833a18ce121ebe9b81ef6d56f45c420f
andi <andi@andilabs.com>
2019-08-20 00:27:47 +0200

    updated insta token

--- js/my-instafeed.js
+++ js/my-instafeed.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
  var tutorialFeed = new Instafeed ({
   get: 'user',
   userId: '2362436988',
-  accessToken: '2362436988.7179641.0864391817f0409781706206c66af726',
+  accessToken: '2362436988.7179641.a3b8f099e1044cfa987678ab5a34250c',
   sortBy: 'most-recent',
   resolution: 'standard_resolution',
   template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
